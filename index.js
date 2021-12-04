82023e4c5549580809d565e578ea765bc278cb91
Severo-Yannick <severoyannick@gmail.com>
2021-12-04 17:09:53 +0100

    add reducer

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ const buyPhone = () => {
     type: BUY_PHONE
   }
 }
+
+// REDUCER
+const initialState = {
+  phones : 15
+}
+
+const reducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case BUY_PHONE:
+    return {
+      ...state,
+      phones: state.phones - 1,
+    }
+    default:
+      return state
+  }
+}
